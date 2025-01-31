72054b16d0aafda0649ca646d9503fd4e4908760
stian oye <stianoyejenssen@gmail.com>
2025-01-31 09:21:25 +0100

    Started work on accepting socket connections from the client side

--- src/main/java/no/ntnu/server/ServerApplication.java
+++ src/main/java/no/ntnu/server/ServerApplication.java
@@ -1,7 +1,35 @@
 package no.ntnu.server;
 
+import no.ntnu.constants.Config;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApplication {
+
+
+  public boolean start() {
+    boolean success = true;
+
+    try (ServerSocket serverSocket = new ServerSocket(Config.PORT)) {
+      Socket clientSocket = serverSocket.accept();
+
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      success = false;
+    }
+
+    return success;
+  }
+
+
   public static void main(String[] args) {
-    
+    ServerApplication server = new ServerApplication();
+
+    if (server.start()) {
+      System.out.println("Server started successfully");
+    } else {
+      System.out.println("Server failed to start");
+    }
   }
 }
