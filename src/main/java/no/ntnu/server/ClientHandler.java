924c8d1e486473926ac860adce21afe843f9e08a
Ludvik <ludvik.lund.hole@gmail.com>
2025-01-31 11:50:26 +0100

    added javadoc for clienthandler and multithread clienthandler

--- src/main/java/no/ntnu/server/ClientHandler.java
+++ src/main/java/no/ntnu/server/ClientHandler.java
@@ -6,30 +6,71 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * Represents a client handler for the math server.
+ *
+ * <p>This class is responsible for handling the communication between the math
+ * server and the math client.
+ * <p>It supports sending and receiving messages, as well as acting on the messages received.
+ * <p>Received messages are expected to be in the format "operator number1 number2".
+ * The result of the operation is then sent back to the client.
+ *
+ * @author Ludvik Lund-Hole
+ */
 public class ClientHandler {
   private final PrintWriter writer;
   private final BufferedReader reader;
 
+  /**
+   * Creates a new instance of the ClientHandler class.
+   *
+   * @param socket The socket to communicate with the client.
+   * @throws IOException If an I/O error occurs when creating the input or output stream.
+   */
   public ClientHandler(Socket socket) throws IOException {
 
     this.writer = new PrintWriter(socket.getOutputStream(), true);
     this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
   }
 
+  /**
+   * Sends a message to the client.
+   *
+   * @param message The message to send.
+   */
   public void send(String message) {
     writer.println(message);
   }
 
+  /**
+   * Receives a message from the client.
+   *
+   * @return The message received.
+   * @throws IOException If an I/O error occurs when reading the message.
+   */
   public String receive() throws IOException {
     return reader.readLine();
   }
 
+  /**
+   * Gets message, acts on it and sends the response.
+   *
+   * @throws IOException If an I/O error occurs when sending the response.
+   */
   public void handleMessage() throws IOException{
     String message = receive();
     double response = act(message);
     send(String.valueOf(response));
   }
 
+  /**
+   * Performs the operation specified in the message.
+   *
+   * <p>The message is expected to be in the format "operator number1 number2".
+   *
+   * @param message The message containing the operator and the numbers.
+   * @return The result of the operation.
+   */
   private double act(String message) {
     double response = 0;
     String operator;
--- src/main/java/no/ntnu/server/ClientHandlerMultithreaded.java
+++ src/main/java/no/ntnu/server/ClientHandlerMultithreaded.java
@@ -6,12 +6,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * Represents a client handler for the math server with multithreading.
+ */
 public class ClientHandlerMultithreaded extends ClientHandler implements Runnable{
 
+  /**
+   * Creates a new instance of the ClientHandlerMultithreaded class.
+   *
+   * @param socket The socket to communicate with the client.
+   * @throws IOException If an I/O error occurs when creating the input or output stream.
+   */
   public ClientHandlerMultithreaded(Socket socket) throws IOException {
     super(socket);
   }
 
+  /**
+   * Runs the client handler.
+   */
   @Override
   public void run() {
     try {
