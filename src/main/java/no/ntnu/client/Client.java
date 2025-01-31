84a176e3bd411a302fbce644354c17fca0c33cc9
Kristian <garderkristian@gmail.com>
2025-01-31 09:22:58 +0100

    Fixed compilation error

---
+++ src/main/java/no/ntnu/client/Client.java
@@ -0,0 +1,5 @@
+package no.ntnu.client;
+
+public class Client {
+
+}
--- src/main/java/no/ntnu/client/ClientApplication.java
+++ src/main/java/no/ntnu/client/ClientApplication.java
@@ -1,7 +1,25 @@
 package no.ntnu.client;
 
 public class ClientApplication {
+  boolean running = false;
   public static void main(String[] args) {
-    
+    ClientApplication app = new ClientApplication();
+    app.run();
+  }
+
+  private void run() {
+    running = true;
+    while (running) {
+      printMenu();
+    }
+  }
+
+  private void printMenu() {
+    System.out.println("1: Addition");
+    System.out.println("2: Subtraction");
+    System.out.println("3: Multiplication");
+    System.out.println("4: Division");
+    System.out.println("5: Modulo");
+    System.out.println("6: Specified amount of simultaneous requests");
   }
 }
