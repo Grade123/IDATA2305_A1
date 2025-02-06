fb4f9e900c8bdb64547fb48e333777fa4a5ad61e
stian oye <stianoyejenssen@gmail.com>
2025-02-06 15:17:25 +0100

    Fixed some sonarqube problems

--- src/main/java/no/ntnu/server/ClientHandlerMultithreaded.java
+++ src/main/java/no/ntnu/server/ClientHandlerMultithreaded.java
@@ -1,9 +1,6 @@
 package no.ntnu.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
--- src/main/java/no/ntnu/server/Server.java
+++ src/main/java/no/ntnu/server/Server.java
@@ -7,17 +7,17 @@ import java.net.Socket;
 public class Server {
   private ServerSocket serverSocket;
   private boolean running = false;
-  private final int PORT;
+  private final int port;
 
-  public Server(int PORT) {
-    this.PORT = PORT;
+  public Server(int port) {
+    this.port = port;
   }
 
   private boolean connect() {
     boolean success = true;
 
     try {
-      serverSocket = new ServerSocket(this.PORT);
+      serverSocket = new ServerSocket(this.port);
     } catch (Exception e) {
       System.out.println(e.getMessage());
       success = false;
